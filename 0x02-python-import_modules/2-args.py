92f59367c588873844a583b62b2a9b4ad70e4a33
NidaaChok <nidaachok100@gmail.com>
2023-06-08 21:49:10 +0000

    prints the number of and the list of its arguments.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,8 +9,7 @@ if __name__ == "__main__":
         print("0 arguments.")
     elif argc == 2:
         print("1 argument:")
-	print("1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(argc - 1))
-	for i in range(1, argc):
-            print("{}: {}".format(i, sys.argv[i]))
+    for i in range(1, argc):
+        print("{}: {}".format(i, sys.argv[i]))
